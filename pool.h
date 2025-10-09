c472b745f09c0261f7a2d9a46ca3cf0f4ff3f370
higaredajra <higaredajra@msoe.edu>
2025-10-09 13:20:46 -0500

    Stub pool.h

--- pool.h
+++ pool.h
@@ -4,5 +4,30 @@
 
 #ifndef MEETINGUP_POOL_H
 #define MEETINGUP_POOL_H
+#include <string>
+
+using namespace std;
+
+class Student {
+public:
+  Student(string name);
+  Student(string name, int birthdate);
+
+  string printableDescription();
+  bool matches(Student *otherStu);
+};
+
+class Pool {
+public:
+  Pool();
+  Pool(string studentName, string studentBirthdate);
+
+  void add(string s);
+  void add(string s, int n);
+  void readStudents();
+  void printMatches(Student *stu);
+  void printMatches(Pool *otherPool);
+  bool empty();
+};
 
 #endif //MEETINGUP_POOL_H
